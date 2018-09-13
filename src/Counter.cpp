5fbbb4ed8e8413f6aab7b4e6673bae57d15b7659
levongh <levon_ghukasyan@live.com>
2018-09-13 11:00:49 +0400

    added Counter class to track and update game state

---
+++ src/Counter.cpp
@@ -0,0 +1,98 @@
+#include "Counter.h"
+#include "UIManager.h"
+#include "GameTable.h"
+
+Counter::Counter(GameManager* mgr)
+    : m_mgr(mgr)
+{
+    unsigned m_aliensMove     = 0;
+    unsigned m_aliensShot     = 0;
+    unsigned m_playerShot     = 0;
+    unsigned m_ufoMove        = 0;
+    unsigned m_titleAnimation = 0;
+    unsigned m_gameOver       = 0;
+}
+
+void Counter::reset()
+{
+    m_aliensMove = 0;
+    m_aliensShot = 0;
+    m_playerShot = 0;
+    m_ufoMove    = 0;
+}
+
+void Counter::updateCounter()
+{
+    if (m_aliensShot++ >= 5) {
+        m_aliensShot = 0;
+    }
+    if (m_playerShot++ >= 1) {
+        m_playerShot = 0;
+    }
+    if (m_aliensMove++ >= m_mgr->confMgr->getWeite()) {
+        m_aliensMove = 0;
+    }
+    if (m_ufoMove++ >= 3) {
+        m_ufoMove = 0;
+    }
+}
+
+void Counter::animationUpdate()
+{
+    if (m_titleAnimation == 0) {
+        m_mgr->uiMgr->titleScreenDisplay();
+    }
+    if (m_titleAnimation++ >= 6) {
+        m_titleAnimation = 0;
+    }
+}
+
+void Counter::checkGameOver()
+{
+    if (m_gameOver == 100) {
+        m_mgr->uiMgr->battleFieldClear();
+        m_mgr->confMgr->setMode(Mode::HIGHSCORE);
+        m_gameOver = 0;
+    } else {
+        m_mgr->uiMgr->gameOverDisplay();
+        ++m_gameOver;
+    }
+}
+
+void Counter::checkUFO()
+{
+    if (m_ufoMove == 0 &&
+            m_mgr->gameTable->getUFO()->show() == 1) {
+        m_mgr->gameTable->getUFO()->moveLeft();
+    }
+}
+
+void Counter::checkAliensShot()
+{
+    if (m_aliensShot == 0 &&
+            m_mgr->gameTable->getAliens()->missileMove() == 1) {
+        m_mgr->confMgr->setLive(m_mgr->confMgr->getLive() - 1);
+        m_mgr->drawscore();
+        m_mgr->gameTable->getPlayer()->explode();
+        if (m_mgr->confMgr->getLevel() == 0) {
+            m_mgr->confMgr->setMode(Mode::GAME_OVER);
+        }
+    }
+}
+
+void Counter::checkPlayerShot()
+{
+    if (m_playerShot == 0 &&
+            m_mgr->gameTable->getPlayer()->moveMissile() == 1) {
+        m_mgr->confMgr->setMode(Mode::NEXT_LEVEL);
+    }
+}
+
+void Counter::checkAliensMove()
+{
+    if (m_aliensMove == 0 &&
+            m_mgr->gameTable->getAliens()->move() == 1) {
+        m_mgr->confMgr->setLive(0);
+        m_mgr->confMgr->setMode(Mode::GAME_OVER);
+    }
+}
---
+++ src/Counter.h
@@ -0,0 +1,69 @@
+#include "common.h"
+#include "GameManager.h"
+#include "ConfigurationManager.h"
+
+/**
+ * @class  Counter
+ * @file   Counter.h
+ * @brief  Counter class designed to calculate and update game state
+ * @author Levon Ghukasyan
+ */
+
+class Counter
+{
+public:
+    Counter(GameManager* mgr);
+
+    /**
+     * @brief reset
+     */
+    void reset();
+
+    /**
+     * @brief updateCounter function is updateteing the counter data
+     */
+    void updateCounter();
+
+    /**
+     * @brief animationUpdate functions checks if neded titel display animation and updates title screen
+     */
+    void animationUpdate();
+
+    /**
+     * @brief checkGameOver functions checks if the game should be in GAME_OVER state
+     */
+    void checkGameOver();
+
+    /**
+     * @brief checkUFO functions checks if UFO is movable if so it moves
+     */
+    void checkUFO();
+
+    /**
+     * @brief checkAliensShot checks if alien shotted the player is the player live is expiered
+     *        then the game goes to the GAME_OVER game
+     */
+    void checkAliensShot();
+
+    /**
+     * @brief checkPlayerShot
+     */
+    void checkPlayerShot();
+
+    /**
+     * @brief checkAliensMove function checks if allien can move or not
+     *        if the alienc can not move the game is moving to GAME_OVER state
+     */
+    void checkAliensMove();
+
+private:
+    unsigned m_aliensMove;
+    unsigned m_aliensShot;
+    unsigned m_playerShot;
+    unsigned m_ufoMove;
+    unsigned m_titleAnimation;
+    unsigned m_gameOver;
+
+private:
+    GameManager* m_mgr;
+};
